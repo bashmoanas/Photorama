a673db21291ced5ae66b44643ff31142a004f1c7
Anas Bashandy <anasmbash@icloud.com>
2023-03-02 12:17:23 +0200

    Create an EndPoint enum
    This enum contains all the endpoints this app will support.

--- Photorama/FlickrAPI.swift
+++ Photorama/FlickrAPI.swift
@@ -7,6 +7,13 @@
 
 import Foundation
 
+/// The end point to hit on the Flickr Server.
+enum EndPoint: String {
+    
+    /// The endpoint to get interesting photos.
+    case interestingPhotos = "flickr.interestingness.getList"
+}
+
 /// Resposible for knowing and handling all Flickr-related information.
 ///
 /// This includes how to generate the URLs that the Flickr API expects as well as knowing the format of incoming JSON and how to parse that JSON into the relevant model objects.
