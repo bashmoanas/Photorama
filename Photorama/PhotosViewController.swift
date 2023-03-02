c38b0fcfa921803d5b616a762536e2eef00b08d5
Anas Bashandy <anasmbash@icloud.com>
2023-03-02 09:11:46 +0200

    Delete Storyboard

--- Photorama/PhotosViewController.swift
+++ Photorama/PhotosViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  PhotosViewController.swift
 //  Photorama
 //
 //  Created by Anas Bashandy on 02/03/2023.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class PhotosViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
     }
-
-
+    
 }
-
