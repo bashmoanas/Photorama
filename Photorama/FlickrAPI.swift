b4b79a5723bb54699f5a131cc1d67188da0a820a
Anas Bashandy <anasmbash@icloud.com>
2023-03-02 12:11:45 +0200

    Create the FlickrAPI struct
    This struct will handle any Flickr API related information

---
+++ Photorama/FlickrAPI.swift
@@ -0,0 +1,15 @@
+//
+//  FlickrAPI.swift
+//  Photorama
+//
+//  Created by Anas Bashandy on 02/03/2023.
+//
+
+import Foundation
+
+/// Resposible for knowing and handling all Flickr-related information.
+///
+/// This includes how to generate the URLs that the Flickr API expects as well as knowing the format of incoming JSON and how to parse that JSON into the relevant model objects.
+struct FlickrAPI {
+    
+}
