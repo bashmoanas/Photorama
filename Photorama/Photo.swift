f3ce33e71948ba4417e7b7f99191c26ddd067260
Anas Bashandy <anasmbash@icloud.com>
2023-03-09 13:27:28 +0200

    Conform the Photo struct to Codable to allow decoding from JSON

--- Photorama/Photo.swift
+++ Photorama/Photo.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 /// Represent each photo that is returned from the web service request.
-struct Photo {
+struct Photo: Codable {
     
     /// The photo title.
     let title: String
